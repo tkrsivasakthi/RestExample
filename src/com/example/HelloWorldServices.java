3a0a8ca8b8727147dc06c262bb583b3e0ee6d20c
tkrsivasakthi <tkrsivasakthi@gmail.com>
2016-11-01 11:13:42 +0530

    First Commit

---
+++ src/com/example/HelloWorldServices.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/customer")
+public class HelloWorldServices {
+
+	@GET
+	@Produces("text/html")
+	public Response getLocalCustomer()
+	{
+		String output=" hello, This is local. get customer";
+		return Response.status(200).entity(output).build();
+		
+	}
+	
+	@GET
+	@Path("/list")
+	@Produces("text/html")
+	public Response getCustomerList()
+	{
+		String str= "Hello, customer details from list";
+		return Response.status(200).entity(str).build();
+		
+	}
+	
+	@GET
+	@Path("/add/{a}/{b}")
+	@Produces("text/html")
+	public String addText(@PathParam("a") double a,@PathParam("b") double b)
+	{
+		return (a+b) + "";
+		
+	}
+	
+}
